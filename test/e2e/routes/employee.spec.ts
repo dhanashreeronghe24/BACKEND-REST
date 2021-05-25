5b426c113242bda7d456d8933d7669112256b912
Dhanshree Ronghe <dhanashreeronghe24@gmail.com>
2021-05-25 11:52:28 +0530

    added test case for API

--- test/e2e/routes/employee.spec.ts
+++ test/e2e/routes/employee.spec.ts
@@ -1,7 +1,45 @@
-//sample test case
-describe("e2eTESTS - Employee CRUD", ()=>{   
-    it("shud run sample test", ()=> {
-        expect(5+4).toEqual(9);
+import { Express } from 'express';
+import { createServer, Server } from 'http';
+import supertest from 'supertest';
+import init from '../../../src/app';
+import { closeDBConnection } from '../../../src/db/dbConnection';
+import Employee from '../../../src/types/employee'
+
+
+describe("e2eTESTS - Employee CRUD", ()=>{
+    let app: Express;
+    let server: Server;
+    let generatedId: string;
+    let fakeEmployeeRequest: Employee = {
+      empId: "e3",
+      empName: "Sarah",
+      empDOB: "24-07-1995",
+      empContact: "+1 234 56765 7"
+    }
+
+    beforeEach( async ()=>{
+        app = await init();
+        server = createServer(app);
+    })
+
+    it('should create emp', async () => {
+        //arrange
+        const expectedEmployee: Employee = fakeEmployeeRequest;
+        const req = supertest(server);
+    
+        // act
+        let res = await req.post('/employee')
+          .send(expectedEmployee);
+    
+        // assert
+        expect(res.status).toEqual(200);
+        generatedId = res.body?._id;
+        expect(res.body?.empId).toEqual(expectedEmployee.empId);
+    
+      })
+
+    afterAll(async ()=>{
+        await closeDBConnection();
     })
 
 });
