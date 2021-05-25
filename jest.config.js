ed9a71fab2ca734d3480f97568ccf069be79db2a
Dhanshree Ronghe <dhanashreeronghe24@gmail.com>
2021-05-25 11:27:50 +0530

    added sample test case

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts)$',
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts'],
+    setupFilesAfterEnv: ['./jest.setup.js']
+  };
---
+++ test/e2e/routes/employee.spec.ts
@@ -0,0 +1,7 @@
+//sample test case
+describe("e2eTESTS - Employee CRUD", ()=>{   
+    it("shud run sample test", ()=> {
+        expect(5+4).toEqual(9);
+    })
+
+});
