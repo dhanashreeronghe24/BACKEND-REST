1acef2a041b50634d88620c02fae4ff0dbc715a1
Dhanshree Ronghe <dhanashreeronghe24@gmail.com>
2021-05-26 10:39:05 +0530

    added tes case

--- test/e2e/routes/employee.spec.ts
+++ test/e2e/routes/employee.spec.ts
@@ -6,66 +6,80 @@ import { closeDBConnection } from '../../../src/db/dbConnection';
 import Employee from '../../../src/types/employee'
 
 
-describe("e2eTESTS - Employee CRUD", ()=>{
-    let app: Express;
-    let server: Server;
-    let generatedId: string;
-    let fakeEmployeeRequest: Employee = {
-      empId: "e3",
-      empName: "Sarah",
-      empDOB: "24-07-1995",
-      empContact: "+1 234 56765 7"
-    }
+describe("e2eTESTS - Employee CRUD", () => {
+  let app: Express;
+  let server: Server;
+  let generatedId: string;
+  let fakeEmployeeRequest: Employee = {
+    empId: "e3",
+    empName: "Sarah",
+    empDOB: "24-07-1995",
+    empContact: "+1 234 56765 7"
+  }
 
-    beforeEach( async()=>{
-      app = await init();
-      server = createServer(app);
-    })
+  beforeEach(async () => {
+    app = await init();
+    server = createServer(app);
+  })
 
-    it('Add an employee', async () => {
-        //arrange
-        const expectedEmployee: Employee = fakeEmployeeRequest;
-        const req = supertest(server);
-    
-        // act
-        let res = await req.post('/employee')
-          .send(expectedEmployee);
-    
-        // assert
-        expect(res.status).toEqual(200);
-        generatedId = res.body?._id;
-        expect(res.body?.empId).toEqual(expectedEmployee.empId);
-    
-      })
-      
-      it('should fetch an employee by id', async () => {
-        //arrange
-        const req = supertest(server);
-    
-        // act
-        let res = await req.get(`/employee/${generatedId}`);
-    
-        // assert
-        expect(res.status).toEqual(200);
-        expect(res.body?.empId).toEqual(fakeEmployeeRequest.empId);
-    
-      })
+  it('Add an employee', async () => {
+    //arrange
+    const expectedEmployee: Employee = fakeEmployeeRequest;
+    const req = supertest(server);
 
-      it('Delete an employee', async () => {
-        //arrange
-        const req = supertest(server);
-    
-        // act
-        let res = await req.delete(`/employee/${generatedId}`);
-    
-        // assert
-        expect(res.status).toEqual(200);
-        expect(res.body?.deletedCount).toEqual(1);
-    
-      })
+    // act
+    let res = await req.post('/employee')
+      .send(expectedEmployee);
 
-    afterAll(async ()=>{
-        await closeDBConnection();
-    })
+    // assert
+    expect(res.status).toEqual(200);
+    generatedId = res.body?._id;
+    expect(res.body?.empId).toEqual(expectedEmployee.empId);
+  })
+
+  it('Fetch an employee by id', async () => {
+    //arrange
+    const req = supertest(server);
+
+    // act
+    let res = await req.get(`/employee/${generatedId}`);
+
+    // assert
+    expect(res.status).toEqual(200);
+    expect(res.body?.empId).toEqual(fakeEmployeeRequest.empId);
+  })
+
+  it('Update an employee', async () => {
+    //arrange
+    const req = supertest(server);
+    fakeEmployeeRequest.empId = "e3"
+    fakeEmployeeRequest.empName = "Sarah update";
+    fakeEmployeeRequest.empContact = "+1 234 56765 8";
+    fakeEmployeeRequest.empDOB = "24-07-1995";
+    let expectedEmployee: Employee = fakeEmployeeRequest;
+
+    // act
+    let res = await req.put(`/employee/${generatedId}`).send(expectedEmployee);
+
+    // assert
+    expect(res.status).toEqual(200);
+    expect(res.body?.nModified).toEqual(1);
+  })
+
+  it('Delete an employee', async () => {
+    //arrange
+    const req = supertest(server);
+
+    // act
+    let res = await req.delete(`/employee/${generatedId}`);
+
+    // assert
+    expect(res.status).toEqual(200);
+    expect(res.body?.deletedCount).toEqual(1);
+  })
+
+  afterAll(async () => {
+    await closeDBConnection();
+  })
 
 });
